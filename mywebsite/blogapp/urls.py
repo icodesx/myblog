34a05c3802737e01db022f7f31c70a29ec2486e7
ignaciodb <ignaciodiazbarraza@gmail.com>
2021-01-20 00:11:50 -0300

    now the home page lists all the posts from the database and every post has its own page

--- mywebsite/blogapp/urls.py
+++ mywebsite/blogapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import IndexView, PostView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', IndexView.as_view(), name='index'),
+    path('article/<int:pk>', PostView.as_view(), name='post'),
 ]
--- mywebsite/blogapp/views.py
+++ mywebsite/blogapp/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
 # Create your views here.
 
 
-def index(request):
-    return render(request, 'index.html', {})
+class IndexView(ListView):
+    model = Post
+    template_name = 'index.html'
+
+
+class PostView(DetailView):
+    model = Post
+    template_name = 'post.html'
