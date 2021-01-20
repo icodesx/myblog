c486d8b699f2ccad25fdd14352f084362c09c3ef
ignaciodb <ignaciodiazbarraza@gmail.com>
2021-01-20 18:48:34 -0300

    add post form with bootstrap styles

--- mywebsite/blogapp/views.py
+++ mywebsite/blogapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -17,5 +18,5 @@ class PostView(DetailView):
 
 class AddPostView(CreateView):
     model = Post
+    form_class = PostForm
     template_name = 'add-post.html'
-    fields = '__all__'
