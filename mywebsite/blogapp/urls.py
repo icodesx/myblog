585e938cc4045303ddb040604ed5c20f06bc3199
ignaciodb <ignaciodiazbarraza@gmail.com>
2021-01-20 20:26:50 -0300

    added edit post and delete post

--- mywebsite/blogapp/urls.py
+++ mywebsite/blogapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
-from .views import IndexView, PostView, AddPostView
+from .views import IndexView, PostView, AddPostView, EditPostView, DeletePostView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('article/<int:pk>', PostView.as_view(), name='post'),
     path('add-post/', AddPostView.as_view(), name='add-post'),
+    path('article/<int:pk>/edit', EditPostView.as_view(), name='edit-post'),
+    path('article/<int:pk>/delete>', DeletePostView.as_view(), name='delete-post')
 ]
