1349a1475387251e3f8b09deaa6475428c36830d
ignaciodb <ignaciodiazbarraza@gmail.com>
2021-01-23 17:06:00 -0300

    added unlike button, so now users can unlike a post if they had previously liked the post

--- mywebsite/blogapp/views.py
+++ mywebsite/blogapp/views.py
@@ -10,7 +10,15 @@ from django.http import HttpResponseRedirect
 
 def LikeView(request, pk):
     post = get_object_or_404(Post, id=request.POST.get('post_id'))
-    post.likes.add(request.user)
+    liked = False
+
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
+    else:
+        post.likes.add(request.user)
+        liked = True
+
     return HttpResponseRedirect(reverse('post', args=[str(pk)]))
 
 
@@ -44,10 +52,16 @@ class PostView(DetailView):
         cat_menu = Category.objects.all()
         context = super(PostView, self).get_context_data(*args, **kwargs)
 
-        context['cat_menu'] = cat_menu
         stuff = get_object_or_404(Post, id=self.kwargs['pk'])
         total_likes = stuff.total_likes()
+
+        liked = False
+        if stuff.likes.filter(id=self.request.user.id):
+            liked = True
+
+        context['cat_menu'] = cat_menu
         context['total_likes'] = total_likes
+        context['liked'] = liked
         return context
 
 
